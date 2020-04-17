3a546f5ad4a86134d03554fd961c990238a05563
Adrian Bece <adrian@prototyp.digital>
2020-04-17 14:40:19 +0200

    Update

--- src/modules/score/components/IncrementScore.tsx
+++ src/modules/score/components/IncrementScore.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import { updateScoreHistory } from 'modules/score';
 
-export const IncrementScore = ({ user }: { user: firebase.UserInfo }) => {
+interface Props {
+  user: firebase.UserInfo;
+}
+
+export const IncrementScore: React.FC<Props> = ({ user }: Props) => {
   const onClick = async () => {
     const score = Math.floor(Math.random() * (10 - 1) + 1);
     await updateScoreHistory(user, {
