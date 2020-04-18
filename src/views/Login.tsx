666b5963531fc642412b89641c80a6c2846d9cdb
Adrian Bece <adrian@prototyp.digital>
2020-04-18 14:09:01 +0200

    Added signup link

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import { LoginWithGoogle, LoginWithEmail } from 'modules/user';
 
@@ -6,6 +7,9 @@ const Login: React.FC = () => (
   <>
     <LoginWithEmail />
     <LoginWithGoogle />
+    <div>
+      <Link to="/signup">Sign up</Link>
+    </div>
   </>
 );
 
